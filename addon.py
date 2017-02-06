3768574e667c6b3146034dc2864ae9da91ccdae4
sjoerd ophof <sjoerd ophof>
2017-02-06 15:40:47 +0100

    changed content to 'files' to make plugin compatible with 'Leia'

--- addon.py
+++ addon.py
@@ -14,7 +14,7 @@ pluginurl = sys.argv[0]
 # Get the plugin handle as an integer number.
 pluginhandle = int(sys.argv[1])
 # Let kodi show the streams as a list of files
-xbmcplugin.setContent(pluginhandle, 'video')
+xbmcplugin.setContent(pluginhandle, 'files')
 
 def play_url(url):
     query = {'action': 'play', 'url':url}
